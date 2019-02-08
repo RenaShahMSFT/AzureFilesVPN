3a133bd785c3aef0fcc35f7dc4b07fff432cc595
Rena Shah <rena.shah@microsoft.com>
2019-02-07 19:01:59 -0800

    generatecert

--- generatecert.ps1
+++ generatecert.ps1
@@ -1,4 +1,5 @@
 $parentcert = New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject "CN=P2SRootCert" -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
+$clientcert = New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature -Subject "CN=P2SChildCert" -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation "Cert:\CurrentUser\My" -Signer $parentcert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
 Export-Certificate -Cert $parentcert -FilePath c:\P2SRootCertencoded.cer -NoClobber
 certutil -encode c:\P2SRootCertencoded.cer c:\P2SRootCert.cer 
 Get-Content -Path c:\P2SRootCert.cer
