b72f07d3a0fa9b40ec61921ed219dc7952b1a852
Rena Shah <rena.shah@microsoft.com>
2019-02-15 16:20:52 -0800

    rename parent to root

--- generatecert.ps1
+++ generatecert.ps1
@@ -1,6 +1,6 @@
 # Variables
 
-$parentcertname = "CN=P2SRootCert"
+$rootcertname = "CN=P2SRootCert"
 $clientcertname = "CN=P2SChildCert"
 $certLocation = "Cert:\CurrentUser\My"
 $pathtostoreoutputfiles = "C:\"
@@ -11,14 +11,14 @@ $exportedclientcertpath = $pathtostoreoutputfiles + "P2SClientCert.pfx"
 
 # Create, install and Export Self-Signed Root Certificate Signature
 
-$parentcert = New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject $parentcertname -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation $certLocation -KeyUsageProperty Sign -KeyUsage CertSign
-Export-Certificate -Cert $parentcert -FilePath $exportedencodedrootcertpath -NoClobber
+$rootcert = New-SelfSignedCertificate -Type Custom -KeySpec Signature -Subject $rootcertname -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation $certLocation -KeyUsageProperty Sign -KeyUsage CertSign
+Export-Certificate -Cert $rootcert -FilePath $exportedencodedrootcertpath -NoClobber
 certutil -encode $exportedencodedrootcertpath  $exportedrootcertpath
 Get-Content -Path $exportedrootcertpath
 
 # Create, install and export Client Certificate pfx file
 
-$clientcert = New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature -Subject $clientcertname -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation $certLocation -Signer $parentcert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+$clientcert = New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature -Subject $clientcertname -KeyExportPolicy Exportable -HashAlgorithm sha256 -KeyLength 2048 -CertStoreLocation $certLocation -Signer $rootcert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
 $mypwd = ConvertTo-SecureString -String $clientcertpassword -Force -AsPlainText
 Export-PfxCertificate -FilePath $exportedclientcertpath -Password $mypwd -Cert $clientcert
 
